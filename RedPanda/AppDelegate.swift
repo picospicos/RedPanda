8a3ff9498662ba9b0f30179d9ef467990da9c635
Jerome Frery <jeromefrery@macbook-pro-de-jerome.home>
2018-05-16 22:05:34 +0200

    Initial commit

---
+++ RedPanda/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RedPanda
+//
+//  Created by Jerome Frery on 16/05/2018.
+//  Copyright © 2018 Jerome Frery. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RedPanda/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RedPanda
+//
+//  Created by Jerome Frery on 16/05/2018.
+//  Copyright © 2018 Jerome Frery. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
